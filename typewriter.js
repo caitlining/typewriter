281ba8c9ad71098cd69805e924725d22f2f66a98
caitlining <caitlincing@gmail.com>
2019-09-25 01:18:51 +0000

    Refactoring code to be inside a declared function instead of just code in a file

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,16 @@
 const sentence = "hello there from lighthouse labs";
 
-for (let i = 0; i < sentence.length; i++) {
-  let delayTime = i * 50;
-  setTimeout(() => {
-    process.stdout.write(sentence[i]);
+const typewriter = function(sentence) {
+  for (let i = 0; i < sentence.length; i++) {
+    let delayTime = i * 50;
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+      if (i === sentence.length - 1) {
+        process.stdout.write('\n');
+      }
+    }, delayTime);
+  }
+};
 
-    if (i === sentence.length - 1) {
-      process.stdout.write('\n');
-    }
-  }, delayTime);
-}
+
+typewriter(sentence);
